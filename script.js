e31f1b3d55c4b51bf8ad0ed34fe16a8588cb2de8
dimpukd <92726707+dimpukd@users.noreply.github.com>
2022-01-12 10:41:11 +0530

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-function yourActivity(){
-    fetch("http://www.boredapi.com/api/activity?price=0.0")
-    .then(res => res.json())
-    .then(data =>{
-    document.getElementById('activity').innerText = data.activity;})
-}
